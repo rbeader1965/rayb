ac7afad83d8e99e98fe0e165e5023e2cd5f50d25
RAY <raybeader@hotmail.com>
2022-06-15 07:30:19 -0700

    commit

--- components/Reed9.js
+++ components/Reed9.js
@@ -7,23 +7,23 @@ const Reed9 = () => {
     <h1 className='text-4xl md:text-6xl text-gray-700 font-semibold'>Golf Clash Tour 9</h1>
     <h2 className='text-2xl md:text-3xl text-gray-700 font-semibold'>Katana</h2>
     <button className='bg-gray-600 mt-2 mr-2 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
-     <Link className='text-gray-400' href="/pdfs/misc/wedgesmarlin.pdf">Wedges</Link>
+     <Link className='text-gray-400' href="/pdfs/misc/wedgeskatana.pdf">Wedges</Link>
     </button> 
     <button className='bg-gray-600 mt-3 mr-3 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
-    <Link className='text-zinc-100 ' href="/">Rough Irons</Link>
+    <Link className='text-zinc-100 ' href="/pdfs/misc/roughironkatana.pdf">Rough Irons</Link>
     </button>
     <button className='bg-gray-600 mt-3 mr-3  rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
-        <Link href="/">Sand Clubs</Link>
+        <Link href="/pdfs/misc/sandkatana.pdf">Sand Clubs</Link>
     </button>
     <h2 className='mt-2 text-2xl md:text-3xl text-gray-700 font-semibold'>Titan</h2>
     <button className='bg-gray-600 mt-3 mr-3  rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
-     <Link className='text-gray-400' href="/pdfs/misc/wedgesmarlin.pdf">Wedges</Link>
+     <Link className='text-gray-400' href="/pdfs/misc/wedgestitan.pdf">Wedges</Link>
     </button> 
     <button className='bg-gray-600 mt-3 mr-3 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
-    <Link className='text-zinc-100' href="/">Rough Irons</Link>
+    <Link className='text-zinc-100' href="/pdfs/misc/roughirontitan.pdf">Rough Irons</Link>
     </button>
     <button className='bg-gray-600 mt-4 mr-1 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
-        <Link href="/">Sand Clubs</Link>
+        <Link href="/pdfs/misc/sandtitan.pdf">Sand Clubs</Link>
     </button>
     
    </div>
