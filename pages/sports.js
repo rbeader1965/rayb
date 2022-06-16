920261ae38f037f4e2c6b3f698987b9bc7ed0622
RAY <raybeader@hotmail.com>
2022-06-16 05:22:35 -0700

    update

--- pages/sports.js
+++ pages/sports.js
@@ -1,31 +1,50 @@
+import { createClient } from "@supabase/supabase-js";
 
-const Sports = () => {
+export async function getServerSideProps() {
+  const supabaseAdmin = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+    process.env.SUPABASE_SERVICE_ROLE_KEY || ''
+  )
+  const { data } = await supabaseAdmin
+  .from('sports')
+  .select('*')
+  .order('id');
+  return {
+    props: {
+      data1: data,
+    },
+  }
+}
+const Sports = ({data1}) => {
   return (
     <>
-       <div className="w-screen h-screen bg-cover bg-[url('/34.jpeg')] ">
-       <h1 className="p-4 text-3xl text-orange-500">Sports</h1>
-    <table className=" w-full border-2 text-center text-2xl">
-        <thead>
-            <tr>
-                <th>Date</th>
-                <th>Wager Type</th>
-                <th>Amount</th>
-                <th>Result</th>
-                <th>Overall</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Today</td>
-                <td>Round Robin</td>
-                <td>$25,000</td>
-                <td>Win</td>
-                <td>$150,000</td>
-            </tr>
-        </tbody>
-    </table>
-    </div>
+<div className="bg-slate-500 box-border">
+<table className="w-6/12 m-auto text-sm text-left text-gray-500 dark:text-gray-400">
+  <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+    <tr>
+    <th  scope="col" className="px-6 py-3">Date</th>
+    <th  scope="col" className="px-6 py-3">Wager Type</th>
+    <th  scope="col" className="px-6 py-3">Bet Amount</th>
+    <th  scope="col" className="px-6 py-3">Result</th>
+    <th  scope="col" className="px-6 py-3">Total</th>
+    <th  scope="col" className="px-6 py-3">Running Total</th>
+    </tr>
+  </thead>
+  <tbody>
+      {data1.map((image) => (
+    <tr key={image.id} className="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700">
+        <td  className="px-6 py-4">{image.date}</td>
+        <td  className="px-6 py-4">{image.wager_type}</td>
+        <td  className="px-6 py-4">${image.bet_amount}</td>
+        <td  className="px-6 py-4">{image.result}</td>
+        <td  className="px-6 py-4">${image.total}</td>
+        <td  className="px-6 py-4">${image.running_total}</td>
+    </tr>
 
+      ))}
+  </tbody>
+</table>
+</div>
     </>
  
   )
