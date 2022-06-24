37e653101a5517f10ad2d7734712255fded5e5a7
RAY <raybeader@hotmail.com>
2022-06-24 10:17:47 -0700

    update

--- components/Cards.js
+++ components/Cards.js
@@ -4,11 +4,11 @@ import React from 'react'
 const Cards = () => {
   return (
     <div className="bg-blue-800 grid grid-rows-3 grid-flow-col w-96 h-96 gap-4">
-    <div className="row-span-3">
-      <Image src="/tour6images/ep1.png" alt="null" width={256} height={256} className="" /> 
+    <div className="row-span-3 col-span-2">
+      <Image src="/tour6images/ep1.png" alt="null" width={512} height={720} className="object-fill" /> 
     </div>
-    <div className="col-span-2 bg-red-400">02</div>
-    <div className="row-span-2 col-span-2 bg-indigo-900">03</div>
+    <div className="col-span-1 bg-red-400 w-36">02</div>
+    <div className="row-span-2 col-span-2 bg-indigo-900 ">03</div>
   </div>
   )
 }
--- pages/sports.js
+++ pages/sports.js
@@ -20,7 +20,7 @@ const Sports = ({data1}) => {
     <>
 <div className="bg-slate-500 box-border">
 <table className="w-6/12 m-auto text-sm text-left text-gray-500 dark:text-gray-400">
-  <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+  <thead className="font-barlow text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
     <tr>
     <th  scope="col" className="px-6 py-3">Date</th>
     <th  scope="col" className="px-6 py-3">Wager Type</th>
