80f2848fb2305fc4b913d9d365be07f83aad6f7e
RAY <raybeader@hotmail.com>
2022-07-05 15:52:59 -0700

    commit

--- components/Reed.js
+++ components/Reed.js
@@ -2,31 +2,28 @@ import React from 'react'
 import Link from 'next/link'
 const Reed = () => {
   return (
-    <div className='text-center mb-12 font-roboto space-x-4'>
-    <h5 className='text-base md:text-lg text-gray-700 mb-1'>Welcome to </h5>
-    <h1 className='text-4xl md:text-6xl text-gray-700 font-semibold'>Golf Clash Tour 6</h1>
-    <h2 className='text-2xl md:text-3xl text-gray-700 font-semibold'>Marlin</h2>
-    <button className='bg-gray-400 mt-2 mr-2 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
-     <Link className='text-gray-400' href="/pdfs/misc/wedgesmarlin.pdf">Wedges</Link>
-    </button> 
-    <button className='bg-gray-400 mt-3 mr-3 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
-    <Link className='text-zinc-100' href="/pdfs/misc/roughironmarlin.pdf">Rough Irons</Link>
-    </button>
-    <button className='bg-gray-400 mt-3 mr-3  rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
-        <Link href="/pdfs/misc/sandmarlin.pdf">Sand Clubs</Link>
-    </button>
-    <h2 className='mt-2 text-2xl md:text-3xl text-gray-700 font-semibold'>Range Ball</h2>
-    <button className='bg-gray-400 mt-3 mr-3  rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
-     <Link className='text-zinc-100' href="/pdfs/misc/wedgesrange.pdf">Wedges</Link>
-    </button> 
-    <button className='bg-gray-400 mt-3 mr-3 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
-    <Link className='text-zinc-100' href="/pdfs/misc/roughironrange.pdf">Rough Irons</Link>
-    </button>
-    <button className='bg-gray-400 mt-4 mr-1 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
-        <Link  className='text-zinc-100' href="/pdfs/misc/sandrange.pdf">Sand Clubs</Link>
-    </button>
-    
-   </div>
+    <div className='bg-green-500 flex p-12 justify-center gap-2'>
+    <h2 className='mt-2 text-2xl text-gray-700 font-roboto font-semibold'>Marlin</h2>
+<button className='bg-gray-400 mt-2 mr-2 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
+<Link className='text-gray-400' href="/pdfs/misc/wedgesmarlin.pdf">Wedges</Link>
+</button> 
+<button className='bg-gray-400 mt-3 mr-3 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
+<Link className='text-zinc-100' href="/pdfs/misc/roughironmarlin.pdf">Rough Irons</Link>
+</button>
+<button className='bg-gray-400 mt-3 mr-3  rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
+<Link href="/pdfs/misc/sandmarlin.pdf">Sand Clubs</Link>
+</button>
+<h2 className='mt-2 text-2xl text-gray-700 font-roboto font-semibold'>Range Ball</h2>
+<button className='bg-gray-400 mt-3 mr-3  rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
+<Link className='text-zinc-100' href="/pdfs/misc/wedgesrange.pdf">Wedges</Link>
+</button> 
+<button className='bg-gray-400 mt-3 mr-3 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
+<Link className='text-zinc-100' href="/pdfs/misc/roughironrange.pdf">Rough Irons</Link>
+</button>
+<button className='bg-gray-400 mt-4 mr-1 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
+<Link  className='text-zinc-100' href="/pdfs/misc/sandrange.pdf">Sand Clubs</Link>
+</button>
+</div>
   )
 }
 
--- pages/tours/toursix.js
+++ pages/tours/toursix.js
@@ -1,23 +1,26 @@
 import Image from 'next/image';
+import Data from '../../json/tour6.json'
 import Link from 'next/link';
 import Reed from '../../components/Reed';
-import Data from '../../json/tour6.json'
 const TourSix = () => {
   return (
     <div className='h-screen'>
-            <div className='bg-green-500 grid grid-cols-4 grid-rows-6 p-4 gap-2 font-roboto'>
-     <Reed/>
+       <div className='w-full bg-green-500 text-center font-roboto space-x-4'>
+    <h5 className='text-base md:text-lg text-gray-700 mb-1'>Welcome to </h5>
+    <h1 className='text-4xl md:text-6xl text-gray-700 font-semibold'>Golf Clash Tour 6</h1>
+    </div>
+            <div className='bg-green-500 flex flex-wrap justify-center shrink p-2 gap-2 font-roboto'>
                 {Data.map(person => {
                   return(
                   // eslint-disable-next-line react/jsx-key
-                  <div  className='bg-green-500'>
-                    <div  className='h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden object-cover'>
+                  <div  className='bg-green-500 '>
+                    <div  className='h-full w-512 border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden object-cover'>
                     <Image  src={person.src} alt={person.alt} width={512} height={256}/>
                     <div className='p-2 hover:bg-green-900 hover:text-white transition duration-300 ease-in'>
-                    <h2 className='text-base font-medium text-slate-200 mb-1'>Hole # {person.hole}</h2>
-                        <h1 className='text-2xl font-semibold mb-3'>{person.course}</h1>
-                        <p className='leading-relaxed mb-3'>Blue is for Marlin , Gray is for Range Ball</p>
-                         <div className='flex items-center flex-nowrap gap-2'>
+                    <h2 className='text-base text-center font-medium text-slate-200 mb-1'>Hole # {person.hole}</h2>
+                        <h1 className='text-2xl text-center font-semibold mb-3'>{person.course}</h1>
+                        <p className='leading-relaxed text-center mb-2'>Blue is for Marlin , Gray is for Range Ball</p>
+                         <div className='flex justify-center flex-nowrap gap-2'>
                          <button className='bg-blue-600 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-blue-800'>
                             <a href={person.pdf1}>{person.driver}</a>
                           </button>
@@ -31,7 +34,7 @@ const TourSix = () => {
                             <a href={person.pdf4}>{person.shorthorn}</a>
                           </button>
                         </div>
-                        <div className='mt-2 flex items-center flex-nowrap gap-2'>
+                        <div className='mt-2 flex justify-center flex-nowrap gap-2 mb-4'>
                          <button className='bg-gray-400 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800'>
                             <a href={person.pdf5}>{person.driver}</a>
                           </button>
@@ -50,10 +53,9 @@ const TourSix = () => {
                     </div>
                   )
                 })}
-          
-                
+          </div>
+          <Reed/>
 
-            </div>
 
     </div>
   )
