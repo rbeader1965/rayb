c3639c18c5e0eed3c3da26db077fbf564b7abcfe
RAY <raybeader@hotmail.com>
2022-07-10 14:48:07 -0700

    do something

--- components/Cards.js
+++ components/Cards.js
@@ -5,7 +5,9 @@ const Cards = () => {
   return (
     <div className="bg-blue-800 grid grid-rows-3 grid-flow-col w-96 h-96 gap-4">
     <div className="row-span-3 col-span-2">
-      <Image src="/tour6images/ep1.png" alt="null" width={512} height={720} className="object-fill" /> 
+      <form>
+        <input type="text" />
+      </form>
     </div>
     <div className="col-span-1 bg-red-400 w-36">02</div>
     <div className="row-span-2 col-span-2 bg-indigo-900 ">03</div>
