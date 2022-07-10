8a2778b878ddc35a42a5f531b80e46ff6f2cd275
RAY <raybeader@hotmail.com>
2022-07-10 04:02:21 -0700

    commit

--- components/Reed10.js
+++ components/Reed10.js
@@ -2,11 +2,9 @@ import React from 'react'
 import Link from 'next/link'
 const Reed10 = () => {
   return (
-    <div className='text-center mb-12 font-roboto space-x-4'>
-    <h5 className='text-base md:text-lg text-gray-700 mb-1'>Welcome to </h5>
-    <h1 className='text-4xl md:text-6xl text-gray-700 font-semibold'>Golf Clash Tour 10</h1>
+    <div className='bg-slate-500 flex p-12 justify-center gap-2'>
     <h2 className='text-2xl md:text-3xl text-gray-700 font-semibold'>Titan</h2>
-    <button className='bg-gray-400 mt-2 mr-2 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
+    <button className='bg-gray-400 mt-2 mr-3 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
      <Link className='text-gray-400' href="/pdfs/misc/wedgestitan.pdf">Wedges</Link>
     </button> 
     <button className='bg-gray-400 mt-3 mr-3 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
@@ -22,7 +20,7 @@ const Reed10 = () => {
     <button className='bg-gray-400 mt-3 mr-3 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
     <Link className='text-zinc-100' href="/pdfs/misc/roughironkingmaker.pdf">Rough Irons</Link>
     </button>
-    <button className='bg-gray-400 mt-4 mr-1 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
+    <button className='bg-gray-400 mt-3 mr-3 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
         <Link href="/pdfs/misc/sandkingmaker.pdf">Sand Clubs</Link>
     </button>
     
--- pages/hun.js
+++ pages/hun.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import Cards from '../components/Cards'
-import Tables from '../components/Tables'
 
 const  hun= () => {
   return (
     <div>
-       <Tables/> 
        <Cards/>
     </div>
   )
