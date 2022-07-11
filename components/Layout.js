748d60a958a8a921909b4237963e3a674ab81dcb
RAY <raybeader@hotmail.com>
2022-07-11 14:07:01 -0700

    commit

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import Navbar from './Navbar';
 
 export const Layout = ({children}) => {
   return (
-    <div className='w-full'>
+    <div className='w-screen'>
    <Navbar />
    { children }
     </div>
