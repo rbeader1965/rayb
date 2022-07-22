040a5d263646d335386ad26df0aa133616b5f2a4
RAY <raybeader@hotmail.com>
2022-07-22 08:23:00 -0700

    commit

---
+++ components/Footer.js
@@ -0,0 +1,8 @@
+
+export default function Footer() {
+  return (
+    <div className="bg-teal-500 w-full h-32">
+        <h1>Footer</h1>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -22,8 +22,8 @@ export async function getServerSideProps() {
 export default function Home({data1}) {
   return (
 <>
-<div className="bg-slate-500 w-full md:w-auto flex justify-center">
-  <Ideas data1={data1}/>
+<div className="bg-[url('/34.jpeg')] bg-contain bg-center w-full md:w-auto flex justify-center ">
+  <Ideas data1={data1}  />
 </div>
 
 
