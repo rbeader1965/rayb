692a8672f120a569e749e8ee03ad2447d670672c
RAY <raybeader@hotmail.com>
2022-07-22 11:44:25 -0700

    commit

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,15 @@
 
 export default function Footer() {
   return (
-    <div className="bg-teal-500 w-full h-32">
-        <h1>Footer</h1>
+    <div className="bg-gray-900 text-white">
+        <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[ffffff19] py-7">
+          <h1 className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold md:2/5">
+            <span className="text-teal-400">Free</span> until youre ready</h1>
+        <div>
+        <input type="text" placeholder="Enter your Number" className="text-gray-800 sm:w-72 w-full sm:mr-5 mr-1 lg:mb-0 mb-4 py-2.5 rounded px-2 focus:outline-none" />  
+        <button className="bg-teal-400 hover:bg-teal-500 duration-300 px-5 py-2.5 font-roboto rounded-md text-white md:w-auto w-full">Request Code</button>
+        </div>    
+        </div>
     </div>
   )
 }
