15e4b1c0dbf398dddd5cbbc5916f43a0bb669250
RAY <raybeader@hotmail.com>
2022-07-23 09:53:43 -0700

    commit

--- components/Reed9.js
+++ components/Reed9.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 const Reed9 = () => {
   return (
-    <div className='bg-purple-500 flex p-12 justify-center gap-2'>
+    <div className='bg-zinc-500 flex p-12 justify-center gap-2'>
     <h2 className='mt-2 text-3xl text-gray-700 font-roboto font-semibold'>Kingmaker</h2>
     <button className='bg-gray-600 mt-3 mr-3 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
      <Link className='text-gray-400' href="/pdfs/misc/wedgeskingmaker.pdf">Wedges</Link>
