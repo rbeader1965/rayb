86211a93fa90e6c374131adf8faddb3cebe13035
RAY <raybeader@hotmail.com>
2022-07-28 21:47:03 -0700

    commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import { BeakerIcon} from '@heroicons/react/solid'
 const Navbar = () => {
   return (
-    <div className='relative bg-slate-800 w-full h-16 md:h-auto md:w-auto md:flex justify-between '>
+    <div className='relative bg-slate-800 w-full h-16 md:h-auto md:w-auto md:flex justify-between'>
     <div className="flex flex-wrap justify-items-stretch gap-">
       <ul className='m-4 text-base flex flex-wrap items-center gap-1 md:gap-4 font-roboto'>
       <BeakerIcon className="h-4 w-4 md:h-6 md:w-6 md:m-4 text-blue-500 hover:text-yellow-200"/>
@@ -26,12 +26,12 @@ const Navbar = () => {
       </Link>
       </ul>
    </div>
-      <div className='md:visible invisible flex justify-between'>
+      <div className='md:visible invisible flex items-center justify-between'>
        <Link href="/" >
           <a className="m-4 py-2 px-4 md:text-2xl text-base text-indigo-500 hover:text-blue-200 mr-2">Login</a>
         </Link>
         <Link href="/workHistory">
-         <a  className="m-4 py-2 px-4 md:text-3xl text-base text-yellow-700 bg-yellow-400 rounded hover:bg-indigo-800 hover:text-indigo-500">Sign Up</a>
+         <a  className="m-4 py-2 px-4 md:text-2xl text-base text-yellow-700 bg-yellow-400 rounded-lg hover:bg-blue-800 hover:text-white">Sign Up</a>
         </Link>
       </div>
 
--- components/Socialicons.js
+++ components/Socialicons.js
@@ -1,14 +1,24 @@
 import { BsFacebook,BsTwitter,BsInstagram,BsApple,BsBank } from "react-icons/bs";
-
+import Link from "next/link";
 const Socialicons = () => {
   return (
     <div className="text-teal-500">
-        <span className="p-2 cursor-pointer inline-flex items-center gap-4 rounded-full bg-gray-700 mx-1.5 text-xl hover:text-gray-100 hover:bg-teal-500 duration-300">
-         <BsFacebook />
-         <BsTwitter />
-         <BsInstagram />
-         <BsApple />
-         <BsBank />
+        <span className="p-2 cursor-pointer inline-flex items-center space-x-4 rounded-full bg-gray-700 mx-1.5 text-xl hover:text-gray-100 hover:bg-teal-500 duration-300">
+          <Link href="https://www.facebook.com">
+          <a><BsFacebook /></a>
+          </Link>
+         <Link href="https://twitter.com/?lang=en">
+         <a><BsTwitter/></a>
+         </Link>
+         <Link href="https://www.instagram.com">
+          <a><BsInstagram/> </a>
+         </Link>
+         <Link href="https://www.apple.com">
+         <a><BsApple/></a>
+         </Link>
+         <Link href="https://www.chase.com">
+         <a><BsBank/></a>
+         </Link>
 
         </span>
     </div>
