44befc308f9741afbacbc197dffdbf9bc690f9e0
RAY <raybeader@hotmail.com>
2022-08-02 16:22:41 -0700

    commit

--- components/Reed9.js
+++ components/Reed9.js
@@ -1,8 +1,17 @@
-import React from 'react'
 import Link from 'next/link'
 const Reed9 = () => {
   return (
     <div className='bg-zinc-500 grid grid-cols-1 md:flex p-12 justify-center gap-2'>
+       <h2 className='mt-2 text-3xl text-gray-700 font-roboto font-semibold'>Navigator</h2>
+    <button className='bg-gray-600 mt-3 mr-3 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
+     <Link className='text-gray-400' href="/pdfs/misc/wedgesnavigator.pdf">Wedges</Link>
+    </button> 
+    <button className='bg-gray-600 mt-3 mr-3 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
+     <Link className='text-zinc-100 ' href="/pdfs/misc/roughironnavigator.pdf">Rough Irons</Link>
+    </button>
+    <button className='bg-gray-600 mt-3 mr-3  rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
+     <Link href="/pdfs/misc/sandnavigator.pdf">Sand Clubs</Link>
+    </button>
     <h2 className='mt-2 text-3xl text-gray-700 font-roboto font-semibold'>Katana</h2>
     <button className='bg-gray-600 mt-3 mr-3 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
      <Link className='text-gray-400' href="/pdfs/misc/wedgeskatana.pdf">Wedges</Link>
