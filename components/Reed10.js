de2b32249a06ee4653178b7817f47af3ea69b116
RAY <raybeader@hotmail.com>
2022-08-01 21:43:26 -0700

    commit

--- components/Reed10.js
+++ components/Reed10.js
@@ -3,8 +3,8 @@ import Link from 'next/link'
 const Reed10 = () => {
   return (
     <div className='bg-slate-500 grid grid-cols-1 md:flex p-12 justify-center gap-2'>
-    <h2 className='text-2xl md:text-3xl text-gray-700 font-semibold'>Titan</h2>
-    <button className='bg-gray-400 mt-2 mr-3 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
+    <h2 className='mt-2 text-2xl md:text-3xl text-gray-700 font-semibold'>Titan</h2>
+    <button className='bg-gray-400 mt-3 mr-3 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
      <Link className='text-gray-400' href="/pdfs/misc/wedgestitan.pdf">Wedges</Link>
     </button> 
     <button className='bg-gray-400 mt-3 mr-3 rounded-full ring-offset-2 ring-2 text-xs p-1 hover:bg-gray-800 hover:text-white'>
---
+++ pages/tours/toursix.js
@@ -0,0 +1,10 @@
+import Toursixe from "../../components/Toursixe"
+const Toursix = () => {
+  return (
+    <div>
+        <Toursixe />
+    </div>
+  )
+}
+
+export default Toursix
